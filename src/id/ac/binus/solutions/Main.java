9d385c7a71b32fe3defc49579507fac22f16dacc
F4E12 <76392444+F4E12@users.noreply.github.com>
2025-04-24 15:44:21 +0700

    fix proposal

--- src/id/ac/binus/solutions/Main.java
+++ src/id/ac/binus/solutions/Main.java
@@ -3,13 +3,12 @@ package id.ac.binus.solutions;
 public class Main {
 
 	public static void main(String[] args) {
+		Address address = new Address("90", 0);
+		Author author = new Author("Kelompok 1", 20, address);
 		Proposal proposal = new Proposal("Projek Code RE", 
-				"Projek ini merupakan tugas dari mata kuliah Code Reengineering LE01",
-				"Kelompok 1", 20, "BINUS Alam Sutera", 90);
-		DisplayProposal displayProposal = new DisplayProposal();
-		displayProposal.displayProposal(proposal);
-		CopyProposal copyProposal = new CopyProposal();
-		copyProposal.copyProposal(proposal);
+				"Projek ini merupakan tugas dari mata kuliah Code Reengineering LE01", author);
+		proposal.displayProposal(proposal);
+		proposal.copyProposal(proposal);
 	}
 
 }
--- src/id/ac/binus/solutions/Proposal.java
+++ src/id/ac/binus/solutions/Proposal.java
@@ -4,10 +4,12 @@ public class Proposal {
 
 	private String proposalName;
 	private String proposalDescription;
+	private Author author;
 	
-	public Proposal(String proposalName, String proposalDescription) {
+	public Proposal(String proposalName, String proposalDescription, Author author) {
 		this.proposalName = proposalName;
 		this.proposalDescription = proposalDescription;
+		this.author = author;
 	}
 
 	public String getProposalName() {
@@ -16,11 +18,12 @@ public class Proposal {
 	public String getProposalDescription() {
 		return proposalDescription;
 	}
+	public Author getAuthor() {
+		return author;
+	}
 
 	public Proposal copyProposal(Proposal proposal) {
-		Proposal newProposal = new Proposal(proposal.getProposalName(), proposal.getProposalDescription(), 
-				proposal.getAuthorName(), proposal.getAuthorAge(), 
-				proposal.getAuthorAddressStreet(), proposal.getAuthorAddressNumber());
+		Proposal newProposal = new Proposal(proposal.getProposalName(), proposal.getProposalDescription(), proposal.getAuthor());
 		
 		return newProposal;
 	}
@@ -28,7 +31,7 @@ public class Proposal {
 	public void displayProposal(Proposal proposal) {
 		System.out.printf("%s - %s by %s - [%d] lives in %s, %d", 
 				proposal.getProposalName(), proposal.getProposalDescription(), 
-				proposal.getAuthorName(), proposal.getAuthorAge(), 
-				proposal.getAuthorAddressStreet(), proposal.getAuthorAddressNumber());
+				proposal.getAuthor().getName(), proposal.getAuthor().getAge(), 
+				proposal.getAuthor().getAddress().getStreet(), proposal.getAuthor().getAddress().getNumber());
 	}
 }
